57dd403aeaf6f500520e20a753abb66939441801
varun <varunkovuri5@gmail.com>
2023-03-23 13:17:49 +0530

    loginpagefinal

---
+++ loginpagescript.js
@@ -0,0 +1,12 @@
+function validate(){
+    var username=document.getElementById("uname");
+    var password=document.getElementById("pass");
+    if(username.value=="" || username.password==""){
+        alert("No blanks are allowed");
+        return false;
+    }
+    else{
+        true;
+
+    }
+}
