f382c42a8f721adfe77e12d5a7c9f752000aca19
AravindBalamaisi <goudaravind04@gmail.com>
2023-04-02 00:38:31 +0530

    registration and login API's

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session, render_template
 
 import mysql.connector
 from mysql.connector import errorcode
+import datetime
 
 app = Flask(__name__)
 
@@ -30,6 +31,7 @@ def test_db_connection():
 def create_db_connection():
     print("Connected")
     try:
+
         cnx = mysql.connector.connect(**db_config)
         print("connected")
         return cnx
@@ -41,106 +43,63 @@ def create_db_connection():
         else:
             print(err)
 
-#     cnx = create_db_connection()
-#     cursor = cnx.cursor()
-#     query = "SELECT * FROM personal_info"
-#     user_2 = cursor.fetchall()
-#     print("user_2")
+
+# @app.route("/" , methods = ["GET"])
+# def example_template():
+#     return render_template("index.html")
 
 
 # registration API
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register():
     print("Register")
-    if request.headers.get('Content-Type') != "application/json":
-        return 'Error: Request must be JSON format', 400
-    data = request.get_json()
-    firstname = data['firstname']
-    lastname = data['lastname']
-    gender = data['gender']
-    dob = data['dob']
-    email = data['email']
-    mobile_number = data['mobile_number']
-    password = data['password']
-    confirm_password = data['confirm_password']
-    if password != confirm_password:
-        return jsonify({'message': 'Passwords do not match'}), 400
-    cnx = create_db_connection()
-    cursor = cnx.cursor()
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        gender = request.form['gender']
+        dob = request.form['dob']
+        email = request.form['email']
+        mobile_number = request.form['mobile-number']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if password != confirm_password:
+            return jsonify({'message': 'Passwords do not match'})
+        cnx = create_db_connection()
+        cursor = cnx.cursor()
+        last_updated = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        query = "INSERT INTO user_table(email,password,is_active,last_updated,HasContent) VALUES (%s, %s, %s, %s, %s)"
+        values = (email, password, 1, last_updated, 0)
+        cursor.execute(query, values)
+        cnx.commit()
+        cnx.close()
+        return jsonify({'status': 201, 'success': 'True', 'message': 'User registered successfully'})
 
-    query = "INSERT INTO signup (firstname, lastname, gender, dob, email, mobile_number, password, confirm_password) VALUES (%s, %s, %s, %s, %s, %s, %s,%s)"
-    values = (firstname, lastname, gender, dob, email, mobile_number, password, confirm_password)
-    cursor.execute(query, values)
-    cnx.commit()
-    cnx.close()
-    return jsonify({'status': 201, 'success': 'True', 'message': 'User registered successfully'})
+
+global user_id
 
 
 # login API
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['POST'])
 def login():
     print('login started')
-    # if request.headers.get('Content-Type') != 'application/json':
-    #  return 'Error: Request must be JSON format', 400
-    data = {'email': 'aravind@gmail.com', 'password': 'Arand@123'}
-    print(data['email'])
-    # data_1 = request.get_json(data=data)\
-    # print(data_1)
-    try:
-        email_1 = data['email']
-        print(email_1)
-        password_1 = data['password']
-    except KeyError:
-        # Handle the error caused by missing or invalid data in the request payload
-        return jsonify({'status': 400, 'success': 'False', 'message': 'Missing or invalid data'})
-    cnx = create_db_connection()
-    cursor = cnx.cursor()
-    query = "SELECT * FROM signup"
-    cursor.execute(query)
-    user_1 = cursor.fetchall()
-    print(user_1)
-    cnx.close()
-    for user in user_1:
-        if user[5] == email_1:
-            if user[6] == password_1:
-                return jsonify({'status': 200, 'success': 'True', 'message': 'User logged in successfully'})
-    return jsonify({'status': 400, 'success': 'False', 'message': 'Invalid credentials'})
-
-    # values = (firstname, lastname, middlename, dob, addressline_1, addressline_2, city, state, country, pincode, phone, email, website)
-    # cursor.execute(query, values)
-
-
-@app.route('/template')
-def personal_info():
-    print("and")
-    cnx = create_db_connection()
-    cursor = cnx.cursor()
-    query = "SELECT * FROM personal_info"
-    cursor.execute(query)
-    var_1 = cursor.fetchone()
-    return jsonify({'firstname': var_1[0], 'lastname': var_1[1], 'middlename': var_1[2], 'dob': var_1[3], 'addressline_1': var_1[4], 'addressline_2': var_1[5], 'city': var_1[6], 'state': var_1[7], 'country': var_1[8], 'pincode': var_1[9], 'phone': var_1[10], 'email': var_1[11], 'website': var_1[12]})
-# @app.route('/template',methods = ['GET','POST'])
-# def personal_info1():
-#     if request.method == ['POST']:
-#         firstname = request.form['firstname']
-#         lastname = request.form['lastname']
-#         middlename = request.form['middlename']
-#         dob = request.form['dob']
-#         addressline_1 = request.form['addressline_1']
-#         addressline_2 = request.form['addressline_2']
-#         city = request.form['city']
-#         state = request.form['state']
-#         country = request.form['country']
-#         pincode = request.form['pincode']
-
-#         phone = request.form['phone']
-#         email = request.form['email']
-#         website = request.form['website']
-#         cnx = create_db_connection()
-#         cursor = cnx.cursor()
-#         query = "INSERT INTO personal_info (firstname, lastname, middlename, dob, addressline_1, addressline_2, city, state, country, pincode, phone, email, website) VALUES (firstname, lastname, middlename, dob, addressline_1, addressline_2, city, state, country, pincode, phone, email, website)"
-#         cnx.commit()
-#         cnx.close()
+    if request.method == 'POST':
+        try:
+            email_1 = request.form['email']
+            print(email_1)
+            password_1 = request.form['password']
+        except KeyError:
+            # Handle the error caused by missing or invalid data in the request payload
+            return jsonify({'status': 400, 'success': 'False', 'message': 'Missing or invalid data'})
+        cnx = create_db_connection()
+        cursor = cnx.cursor()
+        query = "SELECT * FROM user_table WHERE email=%s and password=%s, (email, password)"
+        cursor.execute(query)
+        user = cursor.fetchone()
+        cnx.close()
+        if user:
+            return jsonify({'message': 'User logged in successfully'})
+        else:
+            return jsonify({'message': 'Invalid credentials'})
 
 
 if __name__ == '__main__':
