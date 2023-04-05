be54a5edcc2c0243394e0ec57dc82f4aa6e2566b
varun <varunkovuri5@gmail.com>
2023-04-05 16:54:13 +0530

    loginscriptfinal

--- loginpagescript.js
+++ loginpagescript.js
@@ -1,97 +1,50 @@
-// const validate = async () => {
-//     var username=document.getElementById("uname").value;
-//     var password=document.getElementById("pass").value;
-//     if(username === "" ||password === ""){
-//         alert("No blanks are allowed");
-//     }
-//     else{
-//         const response = await fetch('/login', {
-//             method: 'POST',
-//             headers: {
-//               'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify({ username, password })
-//           });
-        
-//           const data = await response.json();
-//           console.log(data);
-//           if (data.success) {
-//             window.location.href = 'mainpage.html';
-//           } 
-//     }
-// }
+const form = document.getElementById("login");
+const txtEmail = form.querySelector("[name='email']");
+const txtPassword = form.querySelector("[name='Password']");
 
-const baseUrl = "http://localhost:5501";
-const ApiUrls = {
-
-  LOGIN: `${baseUrl}/login`
-  
-  };
-
-
-
-const form=document.getElementById("login");
-const txtemail =form.querySelector("[name='email']");
-const txtpassword =form.querySelector("[name='Password']");
-form.addEventListener("submit",function(event){
+form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const email = txtemail.value;
-  const password = txtpassword.value;
-  const payload={
-    email,password
+
+  const email = txtEmail.value;
+  const password = txtPassword.value;
+  const payload = {
+    email,
+    password
   };
   alert(email + " " + password);
-  window.fetch(
-    window.fetch(ApiUrls.LOGIN,
-    {method : "Post" ,body: JSON.stringify(payload)}
-  ))
-  .then((response) => {
-   if(response.status==true){
-     
-     alert("login successfully")
-     console.log(response);
-     windows.location.href="Signupwithjs.html";
-      
+
+  window.fetch(window?.config?.ApiUrls?.LOGIN, {
+
+    method: "POST",
+
+    body: JSON.stringify(payload),
+
+    headers: {
+
+      'Content-Type': 'application/json'
+
     }
 
- 
+  }).then((response) => {
+
+    console.log({ response });
+
+    if (response.status == true) {
+
+      alert("login successfully")
+
+      console.log(response);
+
+      windows.location.href = window?.config?.AppRoutes?.SIGNUP;
+
+    }
 
   })
-  
-  .catch((error ) =>{
-    alert(error);
-  });
+
+    .catch((error) => {
+      console.log(error);
+      alert(JSON.stringify(payload));
+
+    });
+
 });
-// form.addEventListener("submit", function (event) {
-//     event.preventDefault();
-  
-//    
-  
-// const email=document.getElementById("uname").Value;
-//  const form=document.getElementById("login").Value;
-//     const password = <value>;
-  
-//    
-  
-//     const payload = {
-//       email,
-//       password
-//     };
-  
-//    
-  
-//     window
-//       .fetch("<LoginPageURL>", {
-//         method: "POST",
-//         body: JSON.stringify(payload)
-//       })
-//       .then((response) => {
-//         if (response.success === true) {
-//           window.location.href = AppRoutes.DASHBOARD;
-//         }
-//         console.log({ response });
-//       })
-//       .catch((err) => {
-//         console.log({ err });
-//       });
-//   });
